81f613ecd59b9144a59f4b43a0b93c60a23a67e7
oulige <103018846+oulige@users.noreply.github.com>
2022-08-12 14:19:26 +0800

    Update 9.js

--- paperinfo/9.js
+++ paperinfo/9.js
@@ -1,1 +1,1 @@
-{"code":0,"data":{"id":9,"userId":"oM2Ub0XigV1aMOm7f1e5c5LgczcU","name":"城轨信号工","total":558,"collectNum":0,"nickName":"欧力","version":1,"allowSearch":1,"hasPwd":1,"createdAt":"2020-07-15 21:04:36","source":1},"message":"success"}
+{"code":0,"data":{"id":9,"userId":"oM2Ub0XigV1aMOm7f1e5c5LgczcU","name":"城轨信号工","total":558,"collectNum":0,"nickName":"欧力","version":1,"allowSearch":1,"password":"987178465","hasPwd":1,"createdAt":"2020-07-15 21:04:36","source":1},"message":"success"}
